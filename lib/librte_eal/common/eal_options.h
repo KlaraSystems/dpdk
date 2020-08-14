5744142207e2ac6206dc7a7d76246039b70500af
Mark Johnston <markjdb@gmail.com>
2020-08-14 11:47:05 -0400

    SHM_LARGEPAGE and NUMA support

--- lib/librte_eal/common/eal_options.h
+++ lib/librte_eal/common/eal_options.h
@@ -93,6 +93,8 @@ enum {
 	OPT_NO_TELEMETRY_NUM,
 #define OPT_FORCE_MAX_SIMD_BITWIDTH  "force-max-simd-bitwidth"
 	OPT_FORCE_MAX_SIMD_BITWIDTH_NUM,
+#define	OPT_LARGEPAGE_OBJECT  "largepage-object"
+	OPT_LARGEPAGE_OBJECT_NUM,
 
 	/* legacy option that will be removed in future */
 #define OPT_PCI_BLACKLIST     "pci-blacklist"
--- lib/librte_eal/freebsd/eal_lcore.c
+++ lib/librte_eal/freebsd/eal_lcore.c
@@ -2,23 +2,33 @@
  * Copyright(c) 2010-2014 Intel Corporation
  */
 
-#include <unistd.h>
+#include <sys/param.h>
+#include <sys/cpuset.h>
 #include <sys/sysctl.h>
+#include <unistd.h>
 
 #include <rte_log.h>
 #include <rte_eal.h>
 #include <rte_lcore.h>
 #include <rte_common.h>
 #include <rte_debug.h>
+#include <rte_errno.h>
 
 #include "eal_private.h"
 #include "eal_thread.h"
 
-/* No topology information available on FreeBSD including NUMA info */
+int eal_create_cpu_map(void);
+
+struct cpu_info {
+	int domain;
+};
+
+static struct cpu_info cpu_info[RTE_MAX_LCORE];
+
 unsigned
 eal_cpu_core_id(__rte_unused unsigned lcore_id)
 {
-	return 0;
+	return lcore_id;
 }
 
 static int
@@ -29,16 +39,29 @@ eal_get_ncpus(void)
 	size_t len = sizeof(ncpu);
 
 	if (ncpu < 0) {
-		sysctl(mib, 2, &ncpu, &len, NULL, 0);
+		(void)sysctl(mib, 2, &ncpu, &len, NULL, 0);
 		RTE_LOG(INFO, EAL, "Sysctl reports %d cpus\n", ncpu);
 	}
 	return ncpu;
 }
 
+static int
+eal_get_ndomains(void)
+{
+	static int ndomains = -1;
+	size_t len = sizeof(ndomains);
+
+	if (ndomains < 0) {
+		(void)sysctlbyname("vm.ndomains", &ndomains, &len, NULL, 0);
+		RTE_LOG(INFO, EAL, "Sysctl reports %d NUMA domains\n", ndomains);
+	}
+	return ndomains;
+}
+
 unsigned
 eal_cpu_socket_id(__rte_unused unsigned cpu_id)
 {
-	return 0;
+	return cpu_info[cpu_id].domain;
 }
 
 /* Check if a cpu is present by the presence of the
@@ -49,4 +72,25 @@ eal_cpu_detected(unsigned lcore_id)
 {
 	const unsigned ncpus = eal_get_ncpus();
 	return lcore_id < ncpus;
+}
+
+int
+eal_create_cpu_map(void)
+{
+	cpuset_t set;
+	int cpu, ndomains = eal_get_ndomains();
+
+	for (int i = 0; i < ndomains; i++) {
+		if (cpuset_getaffinity(CPU_LEVEL_WHICH, CPU_WHICH_DOMAIN, i,
+		    sizeof(set), &set) != 0) {
+			rte_errno = errno;
+			return -1;
+		}
+		while (!CPU_EMPTY(&set)) {
+			cpu = CPU_FFS(&set) - 1;
+			CPU_CLR(cpu, &set);
+			cpu_info[cpu].domain = i;
+		}
+	}
+	return 0;
 }
